a680fbcfbb7aa4fa42b5763df36ed83bd5afe1df
Ziezou Claudi <ziezou.claudi24@mhs.uinjkt.ac.id>
2025-06-30 19:41:32 +0700

    nama dosen final

--- dosen.js
+++ dosen.js
@@ -23,4 +23,52 @@ const dosen = [
         nama: "Nuryasin, M.Kom.",
         email: "nuryasin@uinjkt.ac.id"
     },
+    {
+        nama: "Meinarini Catur Utami, M.T.",
+        email: "meinarini@uinjkt.ac.id"
+    },
+    {
+        nama: "A'ang Subiyakto, M.Kom., Ph.D.",
+        email: "aang_subiyakto@uinjkt.ac.id"
+    },
+    {
+        nama: "Rinda Hesti Kusumaningtyas, M.M.S.I.",
+        email: "rinda.hesti@uinjkt.ac.id"
+    },
+    {
+        nama: "Nida'ul Hasanati, S.T., M.M.S.I.",
+        email: "nidaul.hasanati@uinjkt.ac.id"
+    },
+    {
+        nama: "Elvi Fetrina, M.IT.",
+        email: " elvifetrina@uinjkt.ac.id"
+    },
+    {
+        nama: "Evy Nurmiati, S.Kom, M.M.S.I.",
+        email: "evy.nurmiati@uinjkt.ac.id"
+    },
+    {
+        nama: "Yuni Sugiarti, M.Kom.",
+        email: " yuni.sugiarti@uinjkt.ac.id"
+    },
+    {
+        nama: "Elsy Rahajeng, M.T.I.",
+        email: "elsy.rahajeng@uinjkt.ac.id"
+    },
+    {
+        nama: "Eva Khudzaeva, M.Si",
+        email: "Eva Khudzaeva, M.Si"
+    },
+    {
+        nama: "Abdul Mutholib, M.Sc.",
+        email: "abdulmutholib@uinjkt.ac.id"
+    },
+    {
+        nama: "Dr. Nurbojatmiko, M.Kom.",
+        email: "nurbojatmiko@uinjkt.ac.id"
+    },
+    {
+        nama: "Ir. Eri Rustamaji, M.B.A.",
+        email: "Muhamad Nur Gunawan, M.B.A."
+    }
 ]
